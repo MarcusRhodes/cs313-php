a1adadfc89e4ca85502432108e5681621ca66208
marcusBYUI <rho15005@byui.edu>
2017-09-27 13:23:33 -0400

    adding teach03

---
+++ web/teach03/teach03.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title></title>
+</head>
+<body>
+
+User's name: <?php echo $_POST["name"]; ?><br>
+Email address: 
+	<?php 
+	echo $_POST["email"]; 
+	$mail=$_POST["email"]; 
+	echo "<br><a href='mailto:" . $mail . "?body=" . "" . "'>Mailto link</a>"
+	?><br>
+User's major: <?php echo $_POST["major"]; ?><br>
+User's comments: <?php echo $_POST["comment"]; ?> <br>
+User's visited continents: <?php echo $_POST["location1"]; ?> <br>
+<?php echo $_POST["location2"]; ?> <br>
+<?php echo $_POST["location3"]; ?> <br>
+<?php echo $_POST["location4"]; ?> <br>
+<?php echo $_POST["location5"]; ?> <br>
+<?php echo $_POST["location6"]; ?> <br>
+<?php echo $_POST["location7"]; ?> <br>
+
+</body>
+</html>
