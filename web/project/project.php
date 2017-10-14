1b374c3567d399c7b53f2d364293d21ddcf56292
marcusBYUI <rho15005@byui.edu>
2017-10-14 16:27:41 -0400

    project stuff

--- web/project/project.php
+++ web/project/project.php
@@ -8,21 +8,14 @@ session_start();
 <h1>Image Gallery</h1>
 <?php
 	$dbUrl = getenv('DATABASE_URL');
-	if (empty($dbUrl)) {
-		$dbUrl = "postgres://postgres:password@localhost:5432/projectDB";;
-	}
-	try
-	{
-		$user = 'postgres';
-		$password = 'aurelius6908';
-		$db = new PDO('pgsql:host=127.0.0.1;dbname=projectDB', $user, $password);
-		//$db = pg_connect("pgsql:host=127.0.0.1; dbname=projectDB user='$user' password='$password'");
-	}
-	catch (PDOException $ex)
-	{
-		echo 'Error!: ' . $ex->getMessage();
-		die();
-	}
+	$dbopts = parse_url($dbUrl);
+	$dbHost = $dbopts["host"];
+	$dbPort = $dbopts["port"];
+	$dbUser = $dbopts["user"];
+	$dbPassword = $dbopts["pass"];
+	$dbName = ltrim($dbopts["path"],'/');;
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
 //This is the same thing, but for Heroku
 //$dbUrl = getenv('DATABASE_URL');
 
